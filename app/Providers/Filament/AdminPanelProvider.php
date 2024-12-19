2aabbba46c5c3e50ee42123af30e11c19d699fb5
Misael Cabral <misaelrc.dev@gmail.com>
2024-12-19 11:26:31 -0300

    Adicionado o Charts.js e outras pequenas modificações

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Widgets\ReservationsChart;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\Widgets;
@@ -61,8 +62,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                // Widgets\AccountWidget::class,
+                // Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
